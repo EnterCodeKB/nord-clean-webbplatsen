8104c08d3e5043e287e385571ff88db298fc0eef
EnterCode Sweden KB <info@entercode.se>
2025-08-20 21:56:09 +0200

    changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -158,7 +158,9 @@ const Footer = () => {
               </li>
               <li className="flex items-start">
                 <MapPin className="h-5 w-5 mr-2 mt-0.5 flex-shrink-0" />
-                <address className="not-italic">123 45 Stockholm</address>
+                <address className="not-italic">
+                  Barnhusgatan 20, <br /> 111 23 Stockholm
+                </address>
               </li>
             </ul>
           </div>
